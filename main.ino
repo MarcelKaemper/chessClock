94c5238d6cd85c245a0747f9da459e87572e379e
Marcel <marcelkaemper862@gmail.com>
2019-05-05 11:00:48 +0200

    Added setup and game mode

--- main.ino
+++ main.ino
@@ -9,6 +9,9 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 int turn = 0;
 int time1=30, time2=30;
 
+// 0=setup, 1=game
+int mode = 0;
+
 EvtManager mgr;
 
 void setup() {
@@ -28,42 +31,49 @@ void setup() {
   mgr.addListener(new EvtTimeListener(1000, true, (EvtAction)tick));
 }
 
+//Player 1
 bool buttonOne(){
   turn = 1;
   return true;
 }
 
+// Change time
 bool buttonTwo(){
   return true;
 }
 
+// Start/pause
 bool buttonThree(){
+ mode==0?mode=1:mode=0;
   return true;
 }
 
+//Player 2
 bool buttonFour(){
   turn = 0;
   return true;
 }
 
 bool tick(){
-  if(!turn){
-    time1--;
-  }else{
-    time2--;
+  if(mode == 1){
+    if(!turn){
+      time1--;
+    }else{
+      time2--;
+    }
   }
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.println(time1);
-  lcd.setCursor(0,1);
-  lcd.println(time2);
-  if(time1 <= 0 || time2 <= 0){
-    digitalWrite(buzzer, HIGH);
-    delay(500);
-    digitalWrite(buzzer, LOW);
-    delay(2000);
-    mgr.resetContext();
-  }
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(time1);
+    lcd.setCursor(0,1);
+    lcd.print(time2);
+    if(time1 <= 0 || time2 <= 0){
+      digitalWrite(buzzer, HIGH);
+      delay(500);
+      digitalWrite(buzzer, LOW);
+      delay(2000);
+      mgr.resetContext();
+    }
   return true;
 }
 
